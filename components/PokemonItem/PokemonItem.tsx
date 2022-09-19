5fb37049c9c8459335edf80f68d416fc2de91fdd
Julian <julian.costinea@gmail.com>
2022-09-19 20:55:18 +0200

    completed pokedex queries

--- components/PokemonItem/PokemonItem.tsx
+++ components/PokemonItem/PokemonItem.tsx
@@ -17,9 +17,6 @@ const pokemonItem: React.FC<IProps> = ({ url, name }): ReactElement => {
             src="https://images.unsplash.com/photo-1542779283-429940ce8336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
             layout="fill"
           />
-          <div className={classes.overlay}>
-            <div className={classes.overlayText}>Hello World</div>
-          </div>
         </div>
         <h2 className={classes.pokemonItemTitle}>{name}</h2>
         <section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import React, { useState, useEffect, ReactElement, useRef, MutableRefObject } from "react";
+import React, {
+  useState,
+  useEffect,
+  ReactElement,
+  useRef,
+  MutableRefObject,
+} from "react";
 import Head from "next/head";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
+import Image from "next/image";
 
 import styles from "@/pages/index.module.css";
 import type { NextPage } from "next";
@@ -16,13 +23,22 @@ export interface fetchedPokemons {
 export interface Pokemon {
   id: number;
   name: string;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  height: number;
+  weight: number;
+  sprites: {
+    front_default: string;
+  };
 }
 
 const Home: NextPage = () => {
   const randomPokemonNumber = Math.floor(Math.random() * 152);
   const [errorHeader, setErrorHeader] = useState<string>("");
   const [items, setItems] = useState<fetchedPokemons["results"]>([]);
-  const [pokemon, setPokemon] = useState<string>("Pikachu");
   const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
   const [pokemonType, setPokemonType] = useState<string>("");
   const searchTermInputRef = useRef() as MutableRefObject<HTMLInputElement>;
@@ -38,12 +54,23 @@ const Home: NextPage = () => {
   }
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (evt) => {
+    setErrorHeader("");
     evt.preventDefault();
-    const pokemonQuery = searchTermInputRef.current.value.trim() as string;
+    if (!searchTermInputRef.current.value) {
+      setErrorHeader("Enter a valid pokemon name");
+      return;
+    }
+    let pokemonArray = [];
+    const pokemonQuery = searchTermInputRef.current.value
+      .toLowerCase()
+      .trim() as string;
     const newPokemon = await axiosGetJsonData<Pokemon>(
       `https://pokeapi.co/api/v2/pokemon/${pokemonQuery}`
     );
-    console.log(newPokemon.id);
+    pokemonArray.push(newPokemon);
+    setPokemonType(newPokemon.types[0].type.name);
+    setPokemonData(pokemonArray);
+    console.log(newPokemon);
   };
   // function handleSubmit(event: React.FormEventHandler<HTMLFormElement>){
   //   event.preventDefault();
@@ -73,7 +100,7 @@ const Home: NextPage = () => {
   }, []);
 
   pokemons = items
-    .slice(randomPokemonNumber, randomPokemonNumber + 20)
+    .slice(randomPokemonNumber-5, randomPokemonNumber + 15)
     .map((pokemon) => {
       return (
         <PokemonItem url={pokemon.url} name={pokemon.name} key={pokemon.url} />
@@ -88,17 +115,50 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
+        <h1>Kanto Pokedex</h1>
         <form onSubmit={handleSubmit}>
           <label>
             <input
               ref={searchTermInputRef}
               type="text"
-              placeholder="enter pokemon name"
+              placeholder="Enter pokemon name"
             />
           </label>
         </form>
+        <h2 style={{ color: "red" }}>{errorHeader}</h2>
+        {pokemonData.map((data) => {
+          return (
+            <div key={data.id} className={styles.pokedexContainer}>
+              <div className={styles.pokedexImageContainer}>
+                <Image layout="fill" src={data.sprites.front_default} />
+              </div>
+              <div className={styles.divTable}>
+                <div className={styles.divTableBody}>
+                  <div className={styles.divTableRow}>
+                    <div className={styles.divTableCell}>Type</div>
+                    <div className={styles.divTableCell}>
+                      {pokemonType.charAt(0).toUpperCase() +
+                        pokemonType.slice(1)}
+                    </div>
+                  </div>
+                  <div className={styles.divTableRow}>
+                    <div className={styles.divTableCell}>Height</div>
+                    <div className={styles.divTableCell}>{`${
+                      data.height * 10
+                    } cm`}</div>
+                  </div>
+                  <div className={styles.divTableRow}>
+                    <div className={styles.divTableCell}>Weight</div>
+                    <div className={styles.divTableCell}>{`${
+                      data.weight / 10
+                    } kg`}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
         <h1 className={styles.title}>Other popular pokemon</h1>
-        <h2>{errorHeader}</h2>
         <div className={styles.pokemonContainer}>{pokemons}</div>
       </main>
     </div>
